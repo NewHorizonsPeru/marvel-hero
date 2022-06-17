cd4ae3406d78b11d629d571ee5bb1d87b832bb95
Renato De la Rosa <rdelarosa@develovers.com.pe>
2022-06-16 20:22:17 -0500

    Fragmentos y React.createElement

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('app'));
+
+const elementoJsx = (
+  <>
+    <div className="container">
+      <h1>Aprendiendo React usando JSX</h1>
+      <h3>New Horizons</h3>
+    </div>
+    <div>div2</div>
+  </>
 );
+
+const elementReact = React.createElement(
+  'div',
+  { className: 'container' },
+  React.createElement(
+    'h1',
+    null,
+    'Aprendiendo React usando React.createElement'
+  ),
+  React.createElement('h3', null, 'New Horizons')
+);
+
+root.render(elementoJsx);
