c3f885d711f30754ec8b825bcc9461d62ebe2298
Renato De la Rosa <rdelarosa@develovers.com.pe>
2022-06-21 20:13:12 -0500

    Estados Independientes

--- src/components/FormHero.jsx
+++ src/components/FormHero.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 class FormHero extends React.Component {
+  state = {
+    counter: 0,
+  };
   render() {
     return (
       <div className="col-sm-5">
@@ -9,56 +12,14 @@ class FormHero extends React.Component {
           <div className="panel panel-body form-horizontal payment-form">
             <div className="form-group">
               <label htmlFor="heroName" className="col-sm-3 control-label">
-                Nombre Heroe
+                Contador 1
               </label>
               <div className="col sm-9">
                 <input
                   type="text"
                   className="form-control"
-                  name="heroName"
-                  value={this.props.hero.heroName}
-                  onChange={this.props.onChangeState}
-                />
-              </div>
-            </div>
-            <div className="form-group">
-              <label htmlFor="realName" className="col-sm-3 control-label">
-                Nombre Real
-              </label>
-              <div className="col sm-9">
-                <input
-                  type="text"
-                  className="form-control"
-                  name="realName"
-                  value={this.props.hero.realName}
-                  onChange={this.props.onChangeState}
-                />
-              </div>
-            </div>
-            <div className="form-group">
-              <label htmlFor="photoUrl" className="col-sm-3 control-label">
-                Imagen
-              </label>
-              <div className="col sm-9">
-                <input
-                  type="text"
-                  className="form-control"
-                  name="imageUrl"
-                  value={this.props.hero.imageUrl}
-                  onChange={this.props.onChangeState}
-                />
-              </div>
-            </div>
-            <div className="form-group">
-              <label htmlFor="description" className="col-sm-3 control-label">
-                Reseña
-              </label>
-              <div className="col sm-9">
-                <textarea
-                  className="form-control"
-                  name="description"
-                  value={this.props.hero.description}
-                  onChange={this.props.onChangeState}
+                  name="txtNumberIncrease"
+                  value={this.state.counter}
                 />
               </div>
             </div>
@@ -67,10 +28,14 @@ class FormHero extends React.Component {
               <div className="col sm-12 text-left">
                 <button
                   type="button"
-                  className="btn btn-lg btn-primary mr-1"
-                  onClick={this.props.onClickSaveHero}
+                  className="btn btn-lg btn-success mr-1"
+                  onClick={() => {
+                    this.setState({
+                      counter: this.state.counter + 1,
+                    });
+                  }}
                 >
-                  Guardar
+                  Incrementar
                 </button>
               </div>
             </div>
--- src/components/PreviewHero.jsx
+++ src/components/PreviewHero.jsx
@@ -1,49 +1,54 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import '../styles/Hero.css';
 
 class PreviewHero extends React.Component {
+  state = {
+    counter: 50,
+  };
+
+  handleClickDecrease = () => {
+    this.setState({
+      counter: this.state.counter - 1,
+    });
+  };
+
   render() {
     return (
       <div className="col-sm-7">
         <br />
-        <br />
-        <div className="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-          <div className="col p-4 d-flex flex-column position-static">
-            <strong className="d-inline-block mb-2 text-danger">MARVEL</strong>
-            <h3 className="mb-0">{this.props.heroName}</h3>
-            <div className="mb-1 text-muted">{this.props.realName}</div>
-            <div className="mb-1 text-muted">{this.props.description}</div>
-          </div>
-          <div className="col-auto d-none d-lg-block hero-img">
-            <img
-              className="bd-placeholder-img"
-              width="200"
-              height="250"
-              src={this.props.imageUrl}
-              alt="photoUrl"
-            />
+        <div className="panel panel-default">
+          <div className="panel panel-body form-horizontal payment-form">
+            <div className="form-group">
+              <label htmlFor="heroName" className="col-sm-3 control-label">
+                Contador 2
+              </label>
+              <div className="col sm-9">
+                <input
+                  type="text"
+                  className="form-control"
+                  name="txtNumberDecrease"
+                  value={this.state.counter}
+                />
+              </div>
+            </div>
+            <br />
+            <div className="form-group">
+              <div className="col sm-12 text-left">
+                <button
+                  type="button"
+                  className="btn btn-lg btn-primary mr-1"
+                  onClick={this.handleClickDecrease}
+                >
+                  Disminuir
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     );
   }
 }
-
-PreviewHero.propTypes = {
-  heroName: PropTypes.string,
-  realName: PropTypes.string,
-  imageUrl: PropTypes.string,
-  description: PropTypes.string,
-};
-
-PreviewHero.defaultProps = {
-  heroName: 'Capitan America',
-  realName: 'Steve Rogers',
-  imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/91ODZU+Qz-L.jpg',
-  description:
-    'Es un superhéroe ficticio que aparece en los cómics estadounidenses publicados por Marvel Comics. Creado por los historietistas Joe Simon y Jack Kirby, el personaje apareció por primera vez en Captain America Comics #1 (marzo de 1941) de Timely Comics, predecesor de Marvel Comics. ',
-};
 
 export default PreviewHero;
